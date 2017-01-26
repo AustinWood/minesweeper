cfd0b93aec36caae8df585c18ade1aafef998d34
App Academy Student <student@appacademy.io>
2017-01-26 14:42:27 -0800

    Initialize tiles, 10/81 with mines

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,2 +1,9 @@
 class Minesweeper
+
+  def initialize
+  end
+
+end
+
+if __FILE__ == $PROGRAM_NAME
 end
--- tile.rb
+++ tile.rb
@@ -1,2 +1,16 @@
 class Tile
+
+
+  def initialize(mine)
+    @mine = mine
+    @flagged = false
+    @revealed = false
+  end
+
+  def self.random_tiles
+    # TODO Allow user input of constants,
+    # or create CONSTANTS_VARS, MINE_COUNT = 10, BOARD_SIZE = 9
+    tile_arr = [true] * 10 + [false] * (9 ** 2 - 10)
+    tile_arr.shuffle.map { |mine| self.new(mine) }
+  end
 end
