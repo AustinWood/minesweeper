cfaca666884c7bcd6f6fc4a8e21bd8456b9bccb7
App Academy Student <student@appacademy.io>
2017-01-26 15:04:27 -0800

    Populate board using Tile factory method

--- board.rb
+++ board.rb
@@ -1,7 +1,13 @@
+require_relative 'tile.rb'
+
 class Board
   def initialize(size = 9)
-    @grid = Array.new(size) { Array.new(size) }
     @size = size
+    @grid = populate
+  end
+
+  def populate
+    tiles = Tile.random_tiles(@size)
   end
 
 end
--- tile.rb
+++ tile.rb
@@ -10,6 +10,7 @@ class Tile
   def self.random_tiles(board_size)
     # TODO Allow user input of constants,
     # or create CONSTANTS_VARS, MINE_COUNT = 10, BOARD_SIZE = 9
+    puts board_size
     tile_arr = [true] * 10 + [false] * (board_size ** 2 - 10)
     tile_arr.shuffle.map { |mine| self.new(mine) }
   end
